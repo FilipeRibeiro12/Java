db2c37ff9203f96f01d646c84b89f31d21585e2f
unknown <r.filipesouza@gmail.com>
2025-05-09 11:10:17 -0300

    tipo String

--- estrutura-programa-java/src/io/ioNotasEstudo.java
+++ estrutura-programa-java/src/io/ioNotasEstudo.java
@@ -1,5 +1,7 @@
 package io;
 
+import java.util.Scanner;
+
 /*
  * Entrada e Saída (I/O) de dados no Java.
  * 
@@ -7,5 +9,16 @@ package io;
  * Saida de dados: Escrever dados para o usuário.
  */
 public class ioNotasEstudo {
-  
+  public static void main(String[] args) {
+    // Leitura de dados de entrada
+    System.out.println("Digite um número inteiro:");
+    Scanner scanner = new Scanner(System.in);
+    int valorDigitado = scanner.nextInt();
+    System.out.println(valorDigitado);
+    scanner.close();
+
+    // Saida de dados:
+    System.out.printf("O número digitado foi %d", valorDigitado);
+  }
 }
+ 
